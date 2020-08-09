84e7faeebc41c7966276cafc8cffdc1fa49113f9
alienx456 <estebanelias27dev@gmail.com>
2020-08-09 11:42:18 -0500

    Urls modificadas,documentación actualizada

--- api.py
+++ api.py
@@ -2,6 +2,7 @@
 from flask import Flask,request,Response
 from werkzeug.exceptions import HTTPException
 import json
+from datetime import datetime
 
 #MONGO
 from pymongo import MongoClient
@@ -18,9 +19,27 @@ url = "mongodb+srv://"+user+":"+passmongo+"@cluster0.6zcns.mongodb.net/<dbname>?
 app = Flask(__name__)
 
 
+@app.route('/api/v1/entrada/<fecha_inicio>/<fecha_final>', methods=['GET'])
+def buscar(fecha_inicio,fecha_final):
 
-@app.route('/api/v1/entrada', methods=['GET','POST'])
-def admin_request():
+    try:
+        datetime.strptime(fecha_inicio, '%Y-%m-%d')
+        datetime.strptime(fecha_final, '%Y-%m-%d')
+    except ValueError:
+        return Response(status = 400)
+    
+
+    client_response=MongoClient(url)[mongdb][collection].find({"fecha": {"$gte": fecha_inicio, "$lt": fecha_final}},{ "_id": False})
+
+    mylist = [x for x in client_response]
+
+    return Response(response = json.dumps(mylist),status = 200,content_type='application/json')
+
+
+
+
+@app.route('/api/v1/entrada', methods=['POST'])
+def registrar():
 
     if request.get_json() is None:
 
@@ -28,47 +47,21 @@ def admin_request():
     
     else:
 
-        if request.method == 'GET':
-        
-            fechas = request.get_json()
+        data = request.get_json()
 
-            formato_fecha_v1 = ["fecha_inicio","fecha_final"]
+        formato_llegadas_v1 = ["vuelo","fecha","retraso_horas","origen_ciudad","internacional","aerolinea","pasajeros","avion","escala"]
 
-            llaves_fechas = list(fechas.keys())
+        llaves_llegadas = list(data.keys())
 
-            if formato_fecha_v1 == llaves_fechas:
+        if formato_llegadas_v1 == llaves_llegadas:
 
-                client_response=MongoClient(url)[mongdb][collection].find({"fecha": {"$gte": fechas[formato_fecha_v1[0]], "$lt": fechas[formato_fecha_v1[1]]}},{ "_id": False})
+            client_response=MongoClient(url)[mongdb][collection].insert_one(data)
 
-                mylist = [x for x in client_response]
-
-                return Response(response = json.dumps(mylist),status = 200,content_type='application/json')
-
-            else:
-                return get_bad_request_400()
-
+            return Response(status = 201)
 
         else:
+            return Response(status = 400)
 
-            data = request.get_json()
-
-            formato_llegadas_v1 = ["vuelo","fecha","retraso_horas","origen_ciudad","internacional","aerolinea","pasajeros","avion","escala"]
-
-            llaves_llegadas = list(data.keys())
-
-            if formato_llegadas_v1 == llaves_llegadas:
-
-                client_response=MongoClient(url)[mongdb][collection].insert_one(data)
-
-                return Response(status = 201)
-
-            else:
-                return get_bad_request_400()
-
-
-def get_bad_request_400():
-
-    return Response(status = 400)
 
 
 
